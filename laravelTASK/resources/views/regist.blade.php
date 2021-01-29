ec268035a5caa2eefecd94d3ca75fbeacac15932
rm86choppa <rm86.choppa@icloud.com>
2021-01-30 05:14:47 +0900

    'laravel課題２完了'

--- laravelTASK/resources/views/regist.blade.php
+++ laravelTASK/resources/views/regist.blade.php
@@ -4,28 +4,49 @@
 
 @section('title', 'Laravel')
 
-@section('link', 'regist')
-
-@section('link', 'login')
+@section('nav')
+<a href="regist">regist</a>
+<a href="login">login</a>
+@endsection
 
 @section('contents')
 <div class="contents_title">
     <h3>Register</h3>
 </div>
 <div class="items">
-<form class="form_items" action="index.html" method="post">
+<form class="form_items" action="/regist" method="POST">
     @csrf
     <dl>
+        @error('Name')
+        <dt class="error">エラー</dt>
+        <dd class="error">{{$message}}</dd>
+        @enderror
         <dt>Name</dt>
-        <dd><input type='text' name='Name' class="name"></dd>
+        <dd><input type='text' name='Name' class="name" value="{{old('Name')}}"></dd>
+
+        @error('email')
+        <dt class="error">エラー</dt>
+        <dd class="error">{{$message}}</dd>
+        @enderror
         <dt>E-Mail Address</dt>
-        <dd><input type='email' name='email' class="email"></dd>
+        <dd><input type='email' name='email' class="email" value="{{old('email')}}"></dd>
+        
+        @error('pass')
+        <dt class="error">エラー</dt>
+        <dd class="error">{{$message}}</dd>
+        @enderror
         <dt>Password</dt>
-        <dd><input type='password' name='pass' class="pass"></dd>
+        <dd><input type='password' name='pass' class="pass" value="{{old('pass')}}"></dd>
+
+        @error('confirm_password')
+        <dt class="error">エラー</dt>
+        <dd class="error">{{$message}}</dd>
+        @enderror
+        
         <dt>Confirm Password</dt>
-        <dd><input type='password' name='confirm_password' class="confirm_password"></dd>
+        <dd><input type='password' name='confirm_password' class="confirm_password" value="{{old('confirm_password')}}"></dd>
     </dl>
     <button type="submit" class="btn">Register</button>
-    </form>       
+</form>       
 </div>
 @endsection
